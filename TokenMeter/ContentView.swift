f2afb7c820bf966cbe9bbfdfc20738dbd8afde29
Emrah Usar <emrahusar@gmail.com>
2026-03-26 23:31:06 -0400

    Initial Commit

---
+++ TokenMeter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TokenMeter
+//
+//  Created by Emrah Usar on 3/26/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TokenMeter/TokenMeterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TokenMeterApp.swift
+//  TokenMeter
+//
+//  Created by Emrah Usar on 3/26/26.
+//
+
+import SwiftUI
+
+@main
+struct TokenMeterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
